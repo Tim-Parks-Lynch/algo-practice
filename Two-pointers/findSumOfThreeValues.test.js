d15f15fb7fdc159b49f730e2059217f7f56be90f
Tim Lynch/Janssen <tsplynch@gmail.com>
2024-01-12 18:31:21 -0500

    added more tests

--- Two-pointers/findSumOfThreeValues.test.js
+++ Two-pointers/findSumOfThreeValues.test.js
@@ -17,4 +17,24 @@ describe("findSumOfThreeValues", () => {
     const value = findSumOfThreeValues([0, 1, 2, 3, 4, 5], 9);
     expect(value).toBeTruthy();
   });
+  test("[1, -1, 0], target = -1, should return false", () => {
+    const value = findSumOfThreeValues([1, -1, 0], -1);
+    expect(value).toBeFalsy();
+  });
+  test("[3, 7, 1, 2, 8, 4, 5], target = 10, should return true", () => {
+    const value = findSumOfThreeValues([3, 7, 1, 2, 8, 4, 5], 10);
+    expect(value).toBeTruthy();
+  });
+  test("[3, 7, 1, 2, 8, 4, 5], target = 21, should return false", () => {
+    const value = findSumOfThreeValues([3, 7, 1, 2, 8, 4, 5], 21);
+    expect(value).toBeFalsy();
+  });
+  test("[-1, 2, 1, -4, 5, -3], target = -8, should return true", () => {
+    const value = findSumOfThreeValues([-1, 2, 1, -4, 5, -3], -8);
+    expect(value).toBeTruthy();
+  });
+  test("[-1, 2, 1, -4, 5, -3], target = 0, should return true", () => {
+    const value = findSumOfThreeValues([-1, 2, 1, -4, 5, -3], 0);
+    expect(value).toBeTruthy();
+  });
 });
